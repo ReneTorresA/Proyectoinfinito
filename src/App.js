0a56ccc6b6474a913d0147b0a7f519e929d76e9e
ReneTorresA <rene.torres.a@usach.cl>
2022-03-08 10:58:31 -0300

    cambiado BrowserRoute por HashRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Main from './components/Main'
 import './App.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {HashRouter, Route, Routes} from 'react-router-dom'
 import PythonPage from './components/Pages/PythonPage'
 import './components/Pages/Funcioncreadorpy'
 import CalculoIIIPage from './components/Pages/CalculoIIIPage'
@@ -11,7 +11,7 @@ import CalculoIIIPage from './components/Pages/CalculoIIIPage'
 function App() {
   return (
 
-    <BrowserRouter>
+    <HashRouter>
 
       
       <Header/>
@@ -24,12 +24,12 @@ function App() {
         </div>}/>
         
 
-        <Route path='./python/*' element={<PythonPage/>}/>
-        <Route path='./calculo3/*' element={<CalculoIIIPage/>}/>
+        <Route path='/python/*' element={<PythonPage/>}/>
+        <Route path='/calculo3/*' element={<CalculoIIIPage/>}/>
 
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   )
 }
